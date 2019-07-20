e2bdebb5a8210357f8fef7257b4a2486f1dceaa6
Nemanja Stojanovic <nemkest@gmail.com>
2019-07-20 17:25:02 +0200

    Druga Promena
    Dodat TabBarController

---
+++ AudioKnjige/AudioKnjige/AudioKnjige/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  AudioKnjige
+//
+//  Created by Nemanja on 7/20/19.
+//  Copyright © 2019 NemanjaStojanovic. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
